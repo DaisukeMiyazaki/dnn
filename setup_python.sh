e602a175531983989f5741a478fb7ba64085df26
DaisukeMiyazaki <aubozumom@icloud.com>
2022-12-27 08:20:56 +0900

    finished the first video

--- setup_python.sh
+++ setup_python.sh
@@ -5,3 +5,9 @@ pip list
 
 # start ruuning jupyter-notebook
 jupyter-notebook
+
+# カーネル名の一覧を表示する
+jupyter kernelspec list 
+
+# vscode select kernelから出てきた環境を追加する
+# ex.) /Users/daisuke/.local/share/virtualenvs/nn-bsgCLbAH/share/jupyter/kernels/python3
